9856b3a5a33a097876483b25eb45b46c3fa33797
Habemus <hello@habem.us>
2019-08-26 18:29:07 -0300

    header

--- src/header.php
+++ src/header.php
@@ -47,19 +47,19 @@
   	          'menu_id'        => 'main-menu-cta',
   	        ));
   	      ?>
-          <form
-            action=""
-            method="get">
-            <input
-              placeholder="Busque por palavra"
-              name="s"
-              value="<?php echo get_search_query(); ?>">
-            <button
-              type="submit">
-              <?php require('resources/icon/magnifier.svg'); ?>
-            </button>          
-          </form>
   	    </nav>
+        <form
+          action=""
+          method="get">
+          <input
+            placeholder="Busque por palavra"
+            name="s"
+            value="<?php echo get_search_query(); ?>">
+          <button
+            type="submit">
+            <?php require('resources/icon/magnifier.svg'); ?>
+          </button>          
+        </form>
   	  </div>
     </div>
 	</header>
