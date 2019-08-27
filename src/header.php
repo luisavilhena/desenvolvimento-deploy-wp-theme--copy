33f6f53fa4150f796273421e0ec2398ab7cbe397
Habemus <hello@habem.us>
2019-08-27 18:13:37 -0300

    novas cores

--- src/header.php
+++ src/header.php
@@ -23,11 +23,11 @@
 	<?php require('partials/social-meta-tags.php'); ?>
 </head>
 
-<body class="bg-grey" <?php body_class(); ?>>
+<body class="" <?php body_class(); ?>>
 	<!-- <div id="mobile-menu-overlay"></div> -->
-	<header id="main-header">
-    <div class="bg-grey">
-  	  <div class="website-side-padding website-max-width">
+	<header id="main-header" class="website-side-padding website-max-width">
+    <div class="bg-white">
+  	  <div class="dd-width-100">
 
   	    <button id="mobile-menu-trigger">
   	      <span></span>
@@ -41,25 +41,20 @@
   	          'theme_location' => 'main-menu',
   	          'menu_id'        => 'main-menu',
   	        ));
-
-  	        wp_nav_menu(array(
-  	          'theme_location' => 'main-menu-cta',
-  	          'menu_id'        => 'main-menu-cta',
-  	        ));
   	      ?>
+          <form
+            action=""
+            method="get">
+            <input
+              placeholder="Busque por palavra"
+              name="s"
+              value="<?php echo get_search_query(); ?>">
+            <button
+              type="submit">
+              <?php require('resources/icon/magnifier.svg'); ?>
+            </button>          
+          </form>
   	    </nav>
-        <form
-          action=""
-          method="get">
-          <input
-            placeholder="Busque por palavra"
-            name="s"
-            value="<?php echo get_search_query(); ?>">
-          <button
-            type="submit">
-            <?php require('resources/icon/magnifier.svg'); ?>
-          </button>          
-        </form>
   	  </div>
     </div>
 	</header>
